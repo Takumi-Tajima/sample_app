527c60b21091cc4e60bd2c3859b5420a1b9dfb5f
taji <tajima.takumi0421@gmail.com>
2023-08-21 05:15:51 +0000

    Implement basic login

---
+++ app/javascript/application.js
@@ -0,0 +1,22 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "custom/menu"
+// メニュー操作
+
+// トグルリスナーを追加してクリックをリッスンする
+document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
+  
+  let account = document.querySelector("#account");
+  account.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector("#dropdown-menu");
+    menu.classList.toggle("active");
+  });
+});
